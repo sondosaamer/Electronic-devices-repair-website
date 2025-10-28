e0d0f0b41010ab4740b359a67df781d6bef1de7d
Sondos Aamer <sondosaamer12@example.com>
2025-10-29 02:33:47 +0300

    second upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=soumi_api';
+$username = "root";
+$password = "";
+try{
+    $pdo = new PDO($dsn,$username,$password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e){
+    echo "connection failed" . $e->getMessage();    
+}
